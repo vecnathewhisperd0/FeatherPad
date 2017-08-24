4e248e54e4bbd60d26cbf15c2b74d62ec0dc21c3
Tsu Jan <tsujan2000@gmail.com>
2017-08-24 15:07:15 +0430

    Workaround for Qt5 hover bug

--- featherpad/vscrollbar.cpp
+++ featherpad/vscrollbar.cpp
@@ -33,8 +33,13 @@ bool VScrollBar::event (QEvent *event)
 {
     if (event->type() == QEvent::Enter)
         QApplication::setWheelScrollLines (102);
-    else if (event->type() == QEvent::Leave)
+    else if (event->type() == QEvent::Leave
+             /* Apparently, the Qt5 hover bug is never going to be fixed! */
+             || (QApplication::wheelScrollLines() != defaultWheelSpeed
+                 && !rect().contains (mapFromGlobal (QCursor::pos()))))
+    {
         QApplication::setWheelScrollLines (defaultWheelSpeed);
+    }
 
     return QScrollBar::event (event);
 }
