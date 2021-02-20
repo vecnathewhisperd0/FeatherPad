5ff65de2ddda674d3085824c6a49c32222b800d4
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-02-20 20:13:45 +0100

    Weblate commits (#489)
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/featherpad/ja/
    Translation: Tsujan/FeatherPad
    
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>

--- featherpad/data/translations/featherpad_ja.ts
+++ featherpad/data/translations/featherpad_ja.ts
@@ -1151,13 +1151,13 @@
         <location filename="../../fpwin.cpp" line="5249"/>
         <location filename="../../fpwin.cpp" line="5360"/>
         <source>Copy Final Target Path</source>
-        <translation type="unfinished"></translation>
+        <translation>最終的な対象のパスをコピー</translation>
     </message>
     <message>
         <location filename="../../fpwin.cpp" line="5253"/>
         <location filename="../../fpwin.cpp" line="5364"/>
         <source>Open Final Target Here</source>
-        <translation type="unfinished"></translation>
+        <translation>最終的な対象をここで開く</translation>
     </message>
     <message>
         <location filename="../../fpwin.cpp" line="5270"/>
