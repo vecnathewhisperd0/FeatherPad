b66540308c393a00b57d59a46c9b526bd1e06903
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-05-30 22:05:14 +0200

    Weblate commits (#537)
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/featherpad/nl/
    Translation: Tsujan/FeatherPad
    
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>

--- featherpad/data/translations/featherpad_nl.ts
+++ featherpad/data/translations/featherpad_nl.ts
@@ -1449,12 +1449,13 @@ dan verschijnt er een menuknop op de werkbalk.</translation>
         <location filename="../../prefDialog.ui" line="218"/>
         <source>Menubar accelerators are activated by Alt
 and may interfere with customized shortcuts.</source>
-        <translation type="unfinished"></translation>
+        <translation>Menubalksneltoetsen kunnen worden geactiveerd
+met Alt en kunnen botsen met aangepaste sneltoetsen.</translation>
     </message>
     <message>
         <location filename="../../prefDialog.ui" line="222"/>
         <source>Disable menubar accelerators</source>
-        <translation type="unfinished"></translation>
+        <translation>Menubalksneltoetsen uitschakelen</translation>
     </message>
     <message>
         <location filename="../../prefDialog.ui" line="229"/>
